3cc749491952d8a8d4388b5ec74e68ff0306f455
Mark James Thompson <mark.thompson@patentamt.at>
2019-08-03 23:06:56 +0300

    Vignette.

--- R/pricer.R
+++ R/pricer.R
@@ -2,6 +2,7 @@
 #' @description A function to price options. 
 #' @return integer
 #' @param put_call Takes a flag for a put or a call, and returns a different price
+#' @example pricer('P') returns a put option worth 100
 #' @export
 pricer <- function(put_call = 'C'){ 
   if (put_call == 'P') 
@@ -15,6 +16,7 @@ pricer <- function(put_call = 'C'){
 #' @param price Is the current price of the option
 #' @param sigma Is the variance of the mean price, mu, in terms of a probability
 #' @param mu Mean price of the stock over time
+#' @example hedger(price = 100, sigma=.16, mu=100) returns a delta of 0.5
 #' @import stats
 #' @return integer
 hedger <- function(price = price, sigma=.16, mu=price){
@@ -22,5 +24,3 @@ hedger <- function(price = price, sigma=.16, mu=price){
         mean = mu,
         sd=abs(mu)*sigma)
 }
-
-
