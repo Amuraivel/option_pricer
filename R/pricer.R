47101c53a54cc0ea3dc80b3f9739f4860fa68c9a
Mark James Thompson <mark.thompson@patentamt.at>
2019-08-03 16:10:30 +0300

    Updates to the documentation.

--- R/pricer.R
+++ R/pricer.R
@@ -1,10 +1,25 @@
 #' @title pricer
-#' @description A function to price options using classic Black-Scholes
+#' @description A function to price options. 
 #' @return integer
-#' 
+#' @param put_call Takes a flag for a put or a call, and returns a different price
 #' @export
-pricer <- function(){ 
-  100
+pricer <- function(put_call = 'C'){ 
+  if (put_call == 'P') 
+    100
+  else
+    -100
+}
+
+#' @title hedger
+#' @description Computes deltas for a price hedger based on standard normal distribution
+#' @param price Is the current price of the option
+#' @param sigma Is the variance of the mean price, mu, in terms of a probability
+#' @param mu Mean price of the stock over time
+#' @return integer
+hedger <- function(price = price, sigma=.16, mu=price){
+  pnorm(price, 
+        mean = mu,
+        sd=abs(mu)*sigma)
 }
 
 
